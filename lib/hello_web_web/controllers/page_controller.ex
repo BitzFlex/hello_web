261d72a216ced0b1b8a1e3665db29d9315880e50
성백훈 (BH) <bitzflex@gmail.com>
2019-10-07 17:55:41 +0900

    controller 구현

--- lib/hello_web_web/controllers/page_controller.ex
+++ lib/hello_web_web/controllers/page_controller.ex
@@ -4,4 +4,41 @@ defmodule HelloWebWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def hello(conn,_param) do
+    html conn, """
+    <HTML>
+    <HEAD>
+    <TITLE> Hello !! </TITLE> </HEAD>
+    <BODY>
+        <h1> Hello Phoenix Web </h1>
+    </BODY>
+    </HTML>  
+    """
+  end
+
+  def now(conn,_param) do
+    now = :calendar.local_time() |> NaiveDateTime.from_erl! |> NaiveDateTime.to_string
+
+    html conn, """
+    <HTML>
+    <HEAD>
+    <TITLE> Now </TITLE> </HEAD>
+    <BODY>
+        <h1> #{now} </h1>
+    </BODY>
+    </HTML>  
+    """
+  end
+
+
+  def calc(conn,%{"a" => valueA , "b" => valueB}) do
+    sumValue = String.to_integer(valueA) + String.to_integer(valueB)
+    json conn, %{result: :ok, sum: sumValue}       
+  end
+
+  def calc(conn,_param) do
+    json conn, %{result: :error}    
+  end
+
 end
